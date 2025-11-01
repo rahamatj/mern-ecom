497590eaefbc15b1aaf805ff34724f758793e698
rahamatj <32268831+rahamatj@users.noreply.github.com>
2025-11-01 19:20:27 +0600

    fix product overview errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
-import serverConfig from './utils/server.config.js';
+import config from './utils/server.config.js';
 
 const app = express();
 
@@ -18,8 +18,8 @@ app.use((req, res, next) => {
     next();
 });
 
-const PORT = serverConfig().PORT;
-const MONGO_URI = serverConfig().MONGO_URI;
+const PORT = process.env.PORT || config().PORT;
+const MONGO_URI = config().MONGO_URI;
 
 // Root route
 app.get('/', (req, res) => {
