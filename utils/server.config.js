6683c717390bff0d13967b6df6b5062d5733b06d
rahamatj <32268831+rahamatj@users.noreply.github.com>
2025-11-01 19:51:27 +0600

    fix product overview errors

--- utils/server.config.js
+++ utils/server.config.js
@@ -4,12 +4,13 @@ function config() {
     let cfg = []
 
     cfg["dev"] = {
-        API_URL: "http://127.0.0.1:3001",
         MONGO_URI: "mongodb://127.0.0.1:27017/ecom",
-        PORT: 3001
+        PORT: 3001,
+        API_URL: `http://127.0.0.1:${this.PORT}`,
     }
 
     cfg["prod"] = {
+        PORT: 3001,
         API_URL: "https://mern-ecom-9jpw.onrender.com",
         MONGO_URI: "mongodb+srv://rahamatj:162002025@ecom.m8h6nnq.mongodb.net/?appName=ecom"
     }
